3028f6295f87edfec9f943a4831f8aa6287780ab
Mariam Kharebava <mkharebava@gmail.com>
2026-04-28 10:00:00 +0000

    feat: admin dashboard API with statistics

---
+++ WebApiSolution/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using WebApiSolution.Models;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<LogEntry> LogEntries { get; set; }
+        public DbSet<CardEntry> CardEntries { get; set; }
+        public DbSet<PersonalInfoEntry> PersonalInfoEntries { get; set; }
+    }
+}
